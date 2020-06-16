c2df83c7127ae8191a4abcd3ca9c8c7ba094652f
shotahorii <sh.sinker@gmail.com>
2020-06-17 00:10:55 +0100

    bundle

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import LineChart from './components/LineChart';
 
-const server = 'http://ec2-100-26-225-47.compute-1.amazonaws.com/';
+const server = 'http://ec2-23-23-34-98.compute-1.amazonaws.com/';
 
 class App extends React.Component {
 
